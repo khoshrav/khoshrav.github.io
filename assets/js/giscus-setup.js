0aa150d4785ea77f43d0ee8ed15a857d3282f587
khoshrav <khoshrav@users.noreply.github.com>
2026-03-28 05:22:15 +0000

    Deploying to gh-pages from @ khoshrav/khoshrav.github.io@978caea98a2871c448f3360c6f7b7c570db8c97a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "khoshrav/personal_website",
+    "data-repo": "khoshrav/khoshrav.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
